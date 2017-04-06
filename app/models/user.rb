9591f91eb8cda0e21060fc0a69bfd734d4270c90
nttj16 <nttj16@gmail.com>
2017-04-06 11:01:39 +0800

    added facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,21 @@ class User < ApplicationRecord
   validates :email, presence: { message: "You need to type in an email!"}
 
   enum role: ['customer', 'admin']
+  
+  def self.create_with_auth_and_hash(authentication, auth_hash)
+      user = User.create!(name: auth_hash["first_name"]["last_name"], email: auth_hash["extra"]["raw_info"]["email"])
+      user.authentications << (authentication)      
+	
+	return user
+  
+  end
+
+  def fb_token
+    x = self.authentications.where(:provider => :facebook).first
+    return x.token unless x.nil?
+  end
+
 end
+
+
+  
